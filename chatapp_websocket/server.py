6dd1d769e772664e381ff0544d0412d2c8fc9be4
DianaY-a11y <80307545+DianaY-a11y@users.noreply.github.com>
2023-02-19 18:43:16 -0500

    addede server

--- chatapp_websocket/server.py
+++ chatapp_websocket/server.py
@@ -1,7 +1,7 @@
 import sys
 import socket
 import selectors
-import types 
+import types
 import threading
 
 # create dictionary for account information storage
@@ -12,17 +12,18 @@ socket_to_user = {}
 
 # current version command map
 cmap = {
-    "create" : 0,
-    "list" : 1,
-    "login" : 2,
-    "send" : 3,
-    "delete" : 4,
-    "deliver" : 5,
-    "help" : 6,
+    "create": 0,
+    "list": 1,
+    "login": 2,
+    "send": 3,
+    "delete": 4,
+    "deliver": 5,
+    "help": 6,
 }
 
 # Define version of handling client commands
 version = 1
+
 
 def handle_client(clientsocket, addr):
 
@@ -44,14 +45,15 @@ def handle_client(clientsocket, addr):
         # Check correct version in client code
         if (request[0] != str(version)):
             print(request[0])
-            clientsocket.send("Incorrect client software version. Please use an updated version!".encode())
+            clientsocket.send(
+                "Incorrect client software version. Please use an updated version!".encode())
             break
 
         request.pop(0)
 
         # CREATE
         if request[0] == str(cmap['create']):
-            
+
             # Make sure command is correct length
             if len(request) != 3:
                 help(clientsocket=clientsocket)
@@ -61,11 +63,13 @@ def handle_client(clientsocket, addr):
             password = request[2]
 
             if addr in socket_to_user:
-                clientsocket.send("You are already logged into an account!".encode())
+                clientsocket.send(
+                    "You are already logged into an account!".encode())
 
             elif username in accounts:
-                clientsocket.send("Account with username already exists. Try again".encode())
-            
+                clientsocket.send(
+                    "Account with username already exists. Try again".encode())
+
             # If successful create and init a new account
             else:
                 accounts[username] = {}
@@ -75,7 +79,7 @@ def handle_client(clientsocket, addr):
                 accounts[username]["connection"] = None
                 clientsocket.send("Account successfully created".encode())
 
-        # LIST 
+        # LIST
         elif request[0] == str(cmap['list']):
 
             if len(request) >= 3:
@@ -85,20 +89,21 @@ def handle_client(clientsocket, addr):
             # check a list of accounts with wildcard if given
             if len(request) > 1:
                 search_term = request[1]
-                matching_accounts = [username for username in accounts if search_term in username]
-        
+                matching_accounts = [
+                    username for username in accounts if search_term in username]
+
             else:
                 matching_accounts = list(accounts.keys())
-            
+
             if matching_accounts:
                 response = "\n".join(matching_accounts)
                 clientsocket.send(response.encode())
             else:
                 clientsocket.send("No matching accounts".encode())
-        
+
         # LOGIN
-        elif request[0] == str(cmap['login']):     
-            
+        elif request[0] == str(cmap['login']):
+
             if len(request) != 3:
                 help(clientsocket=clientsocket)
                 clientsocket.send('\r\n\r\n'.encode())
@@ -109,7 +114,8 @@ def handle_client(clientsocket, addr):
 
             # Check if logged in already
             if str(addr) in socket_to_user:
-                clientsocket.send("You are already logged into an account!".encode())
+                clientsocket.send(
+                    "You are already logged into an account!".encode())
 
             # Make sure correct account information is provided
             elif username in accounts and accounts[username]["password"] == password:
@@ -120,17 +126,19 @@ def handle_client(clientsocket, addr):
 
                 # Deliver undelivered messages
                 if len(accounts[username]["received_messages"]) >= 1:
-                    clientsocket.send("Account has undelivered messages: ".encode())
-                    deliver(username=username, password=password, clientsocket=clientsocket)
+                    clientsocket.send(
+                        "Account has undelivered messages: ".encode())
+                    deliver(username=username, password=password,
+                            clientsocket=clientsocket)
 
             else:
                 clientsocket.send("Login unsuccessful".encode())
-            
+
             clientsocket.send("\r\n\r\n".encode())
-        
+
         # SEND
         elif request[0] == str(cmap['send']):
-            
+
             if len(request) < 3:
                 help(clientsocket=clientsocket)
                 continue
@@ -138,29 +146,32 @@ def handle_client(clientsocket, addr):
             # Check self socket status
             if (str(addr)) not in socket_to_user:
                 clientsocket.send("Please login to send a message".encode())
-            
+
             else:
                 recipient = request[1]
-                message = " ".join(request[2: ])
+                message = " ".join(request[2:])
 
-                #check user in sequence
+                # check user in sequence
                 if recipient in accounts:
                     # Check if logged in
                     if accounts[recipient]['active'] == True:
                         message_send = f"\nFrom {socket_to_user[str(addr)]}: Message: {message}"
-                        accounts[recipient]["connection"].send(message_send.encode())
+                        accounts[recipient]["connection"].send(
+                            message_send.encode())
                         clientsocket.send("Message delivered!".encode())
 
                     # Otherwise queue the message
                     else:
-                        accounts[recipient]["received_messages"].append([socket_to_user[str(addr)], message])
-                        clientsocket.send("Recipient not online. Will deliver on demand :)".encode())
+                        accounts[recipient]["received_messages"].append(
+                            [socket_to_user[str(addr)], message])
+                        clientsocket.send(
+                            "Recipient not online. Will deliver on demand :)".encode())
                 else:
                     clientsocket.send("Invalid: user not found".encode())
 
         # DELIVER
         elif request[0] == str(cmap['deliver']):
-            
+
             if len(request) != 3:
                 help(clientsocket=clientsocket)
                 clientsocket.send('\r\n\r\n'.encode())
@@ -168,7 +179,8 @@ def handle_client(clientsocket, addr):
 
             username = request[1]
             password = request[2]
-            deliver(username=username, password=password, clientsocket=clientsocket)
+            deliver(username=username, password=password,
+                    clientsocket=clientsocket)
             clientsocket.send('\r\n\r\n'.encode())
 
         # DELETE
@@ -181,27 +193,33 @@ def handle_client(clientsocket, addr):
 
             username = request[1]
             password = request[2]
-            
+
             # Check if another client is loggged in
             if username in accounts and accounts[username]["connection"] is not None and accounts[username]["connection"] != clientsocket:
-                clientsocket.send("Another client is logged into this account!".encode())
-            
+                clientsocket.send(
+                    "Another client is logged into this account!".encode())
+
             # Check if current user is logged in in any capacity
             elif (str(addr) in socket_to_user):
                 if (socket_to_user[str(addr)] == username):
-                    clientsocket.send("You are logged into the account to be deleted. Logout first".encode())
+                    clientsocket.send(
+                        "You are logged into the account to be deleted. Logout first".encode())
                 else:
-                    clientsocket.send("You are logged into a different account. Unable to delete".encode())
+                    clientsocket.send(
+                        "You are logged into a different account. Unable to delete".encode())
 
             # Aim to delete the account
             elif username in accounts and accounts[username]["password"] == password:
                 if len(accounts[username]["received_messages"]) >= 1:
-                    clientsocket.send("Account has undelivered messages: \n".encode())
-                    deliver(username=username, password=password, clientsocket=clientsocket)
+                    clientsocket.send(
+                        "Account has undelivered messages: \n".encode())
+                    deliver(username=username, password=password,
+                            clientsocket=clientsocket)
                 accounts.pop(username)
                 clientsocket.send("Successfully deleted account".encode())
             else:
-                clientsocket.send("Account not found or incorrect password".encode())
+                clientsocket.send(
+                    "Account not found or incorrect password".encode())
 
             clientsocket.send("\r\n\r\n".encode())
 
@@ -210,21 +228,23 @@ def handle_client(clientsocket, addr):
             help(clientsocket=clientsocket)
 
         else:
-            clientsocket.send("Invalid command\nValid commands: create list login send deliver delete help".encode())
+            clientsocket.send(
+                "Invalid command\nValid commands: create list login send deliver delete help".encode())
+
 
 def deliver(username, password, clientsocket):
 
     if (username in accounts and accounts[username]["password"] == password):
-    
+
         for item in accounts[username]["received_messages"]:
             message_send = f"\nFrom {item[0]}: Message: {item[1]}\n"
             clientsocket.send(message_send.encode())
 
         accounts[username]["received_messages"] = []
-    
-    else: 
+
+    else:
         clientsocket.send("Nonexistent account or wrong password".encode())
-        
+
 
 def help(clientsocket):
     clientsocket.send("""
@@ -238,22 +258,23 @@ def help(clientsocket):
     To logout, disconnect and reconnect to the sever!
     """.encode())
 
+
 def run():
 
-    #socket object creation
+    # socket object creation
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #get information of local machine
+    # get information of local machine
     host = socket.gethostname()
     port = 8888
 
-    #bind port
+    # bind port
     serversocket.bind((host, port))
 
-    #listen for connections
+    # listen for connections
     serversocket.listen(5)
     print(f"Server running on port {port}")
-    try: 
+    try:
         while True:
 
             # Accept incoming requests
@@ -261,12 +282,14 @@ def run():
             print("Connection coming from %s" % str(addr))
 
             # Put client in its own thread
-            client_thread = threading.Thread(target=handle_client, args=(clientsocket, addr))
+            client_thread = threading.Thread(
+                target=handle_client, args=(clientsocket, addr))
             client_thread.start()
     except KeyboardInterrupt:
         pass
 
     serversocket.close()
+
 
 if __name__ == "__main__":
     run()
